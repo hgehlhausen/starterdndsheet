49225527d9a334c602f612ec95095b1168b33478
Heath Gehlhausen <heath.gehlhausen@laser2mail.com>
2017-11-13 20:08:56 -0600

    Include angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
